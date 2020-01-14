cc3c134f3eb373f4eab97fcd23904d7a3aa93084
Erik Grove and Krista Rutz <krista@>
2020-01-14 13:24:07 -0800

    add toggle function to activities & image for each

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,42 @@ $(document).ready(function() {
     $("#toggleImg").fadeToggle();
     //$("#toggleImg").fadeOut();
   });
+
+  
+  //for (var i = 1; i <= 9; i++){} 
+
+    $("#surfText").click(function(){
+      $("#surfImage").toggle();
+    });
+
+    $("#barText").click(function(){
+      $("#barImage").toggle();
+    });
+
+    $("#swimText").click(function(){
+      $("#swimImage").toggle();
+    });
+
+    $("#nudismText").click(function(){
+      $("#nudismImage").toggle();
+    });
+
+    $("#shootingRangeText").click(function(){
+      $("#shootingRangeImage").toggle();
+    });
+    $("#conquistadorText").click(function(){
+      $("#conquistadorImage").toggle();
+    });
+
+    $("#danceText").click(function(){
+      $("#danceImage").toggle();
+    });
+
+    $("#exoticText").click(function(){
+      $("#exoticImage").toggle();
+    });
+
+    $("#movieText").click(function(){
+      $("#movieImage").toggle();
+    });
 });
